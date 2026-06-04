deeeec64f26103e703879196a007ff18efc57f57
Yagorocha10 <yagodasilvarocha30@gmail.com>
2026-06-04 18:08:53 -0300

    feat: estrutura inicial do projeto

---
+++ src/main/java/br/com/cinbesa/arquitetura_exemplo/ArquiteturaExemploApplication.java
@@ -0,0 +1,13 @@
+package br.com.cinbesa.arquitetura_exemplo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArquiteturaExemploApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArquiteturaExemploApplication.class, args);
+	}
+
+}
